b6ba10efe882feb598565d102201f0bb9816f97c
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-02-14 15:31:58 +0200

    Add SQL query to retrieve public school districts with above-average per-pupil expenditure and exemplary ratings

--- problems/Lecture 1 - Relating/dese/12.sql
+++ problems/Lecture 1 - Relating/dese/12.sql
@@ -6,6 +6,34 @@
 -- Sort the results first by the percentage of teachers rated exemplary (high to low),
 -- then by the per-pupil expenditure (high to low).
 
+SELECT districts.name, expenditures.per_pupil_expenditure,
+       staff_evaluations.exemplary
+FROM districts
+         JOIN expenditures
+              ON districts.id= expenditures.district_id
+         JOIN staff_evaluations
+              ON districts.id = staff_evaluations.district_id
+
+WHERE
+    districts.type = 'Public School District'
+  AND
+    per_pupil_expenditure > (
+        SELECT AVG(per_pupil_expenditure)
+        FROM expenditures
+                 JOIN districts
+                      ON districts.id= expenditures.district_id
+    )
+  AND
+    exemplary > (
+        SELECT AVG(exemplary)
+        FROM staff_evaluations
+                 JOIN districts
+                      ON staff_evaluations.district_id = districts.id
+    )
+ORDER BY staff_evaluations.exemplary DESC ,
+         expenditures.per_pupil_expenditure DESC;
+
+-- ---> USING ALIASING
     SELECT d.name,
            e.per_pupil_expenditure,
            s.exemplary
@@ -25,34 +53,3 @@
     )
     ORDER BY s.exemplary DESC,
              e.per_pupil_expenditure DESC;
-
-
-
--- SELECT districts.name, expenditures.per_pupil_expenditure,staff_evaluations.exemplary
--- FROM districts
---          JOIN expenditures
---               ON districts.id= expenditures.district_id
---          JOIN staff_evaluations
---               ON districts.id = staff_evaluations.district_id
---
--- WHERE
---     districts.type = 'Public School District'
---   AND
---     per_pupil_expenditure > (
---         SELECT AVG(per_pupil_expenditure)
---         FROM expenditures
---                  JOIN districts
---                       ON districts.id= expenditures.district_id
---         WHERE districts.type = 'Public School District'
---     )
---   AND
---     exemplary > (
---         SELECT AVG(exemplary)
---         FROM staff_evaluations
---                  JOIN districts
---                       ON staff_evaluations.district_id = districts.id
---         WHERE districts.type = 'Public School District'
---     )
--- ORDER BY staff_evaluations.exemplary DESC ,
---          expenditures.per_pupil_expenditure DESC;
-
