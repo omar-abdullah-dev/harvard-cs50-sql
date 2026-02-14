af9769f7e560c5cabc6464601d3d12ac8de56fdd
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-02-14 23:45:42 +0200

    Add SQL queries in 11.sql and 12.sql to find least expensive players per hit and per RBI in 2001

---
+++ problems/Lecture 1 - Relating/moneyball/12.sql
@@ -0,0 +1,48 @@
+-- 12.sql
+-- Hits are great, but so are RBIs!
+-- In 12.sql, write a SQL query to find the players among the 10 least expensive players per hit
+-- and among the 10 least expensive players per RBI in 2001.
+--
+-- Your query should return a table with two columns,
+-- one for the players’ first names
+-- and one of their last names.
+-- You can calculate a player’s salary per RBI by:
+-- dividing their 2001 salary by their number of RBIs in 2001.
+-- You may assume, for simplicity, that a player will only have one salary and one performance in 2001.
+-- Order your results by:
+-- player ID, least to greatest
+-- (or alphabetically by last name, as both are the same in this case!).
+-- Keep in mind the lessons you’ve learned in 10.sql and 11.sql!
+
+--            ((salaries.salary*1.0)/performances.H) AS "dollars per hit",
+--            ((salaries.salary*1.0)/performances.RBI) AS "RBI per hit"
+
+SELECT first_name,last_name FROM (
+                                     SELECT players.first_name,players.last_name
+                                     FROM players
+                                              JOIN salaries
+                                                   ON players.id = salaries.player_id
+                                              JOIN performances
+                                                   ON players.id = performances.player_id
+                                                       AND performances.year= salaries.year
+                                     WHERE salaries.year = 2001
+                                       AND performances.year = 2001
+                                       AND performances.H>0
+                                     ORDER BY ((salaries.salary*1.0)/performances.H) ASC
+                                     LIMIT 10 )
+INTERSECT
+
+SELECT first_name, last_name FROM (
+                                      SELECT players.first_name,players.last_name
+                                      FROM players
+                                               JOIN salaries
+                                                    ON players.id = salaries.player_id
+                                               JOIN performances
+                                                    ON players.id = performances.player_id
+                                      WHERE salaries.year = 2001
+                                        AND performances.year = 2001
+                                        AND performances.RBI>0
+                                      ORDER BY ((salaries.salary*1.0)/performances.RBI) ASC
+                                      LIMIT 10)
+ORDER BY first_name,last_name;
+
