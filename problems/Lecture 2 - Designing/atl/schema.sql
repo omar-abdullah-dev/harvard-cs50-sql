c9ce491eba3d8ef268507f2df4bd70748837e7c6
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-02-17 16:10:45 +0200

    Refactor schema.sql to enhance table definitions for passengers and check-ins

--- problems/Lecture 2 - Designing/atl/schema.sql
+++ problems/Lecture 2 - Designing/atl/schema.sql
@@ -1,34 +1,47 @@
--- passengers:
-    -- id PK
-    -- first_name
-    -- last_name
-    -- age
+-- Passengers:
+-- id PK
+-- first_name TEXT
+-- last_name TEXT
+-- age INTEGER
+CREATE TABLE IF NOT EXISTS  passengers (
+    "id" INTEGER,
+    "first_name" TEXT NOT NULL ,
+    "last_name" TEXT NOT NULL ,
+    "age" INTEGER NOT NULL CHECK ( age>0 ) ,
+    PRIMARY KEY ("id")
+);
 
 -- Check-ins:
-    -- id PK
-    -- passenger_id FK
-    -- flight_id FK
-    -- datetime of the check-in
+-- id PK
+-- passenger_id FK
+-- flight_id FK
+-- datetime of the check-in TEXT
+CREATE TABLE IF NOT EXISTS checkIns(
+     "id" INTEGER,
+     "passenger_id" INTEGER,
+     "flight_id" INTEGER,
+     "datetime" NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY ("id"),
+     FOREIGN KEY (passenger_id) REFERENCES passengers("id"),
+     FOREIGN KEY (flight_id) REFERENCES flights("id")
+);
 
 -- AirLines:
-    -- id PK
-    -- name of the airline
-    -- concourses operates in
+-- id PK
+-- name of the airline TEXT
+
 
 -- Airline_Concourses:
-    -- airline_id (FK)
-    -- concourse
+--  id PK
+-- airline_id (FK)
+-- concourse TEXT IN SET ('A','B','C','D','E','F','T')
+
 
 -- Flights:
-    -- id PK
-    -- flight number
-    -- airline_id FK
-    -- departure airport TEXT
-    -- heading airport TEXT
-    -- departure_datetime
-    -- arrival_datetime
+-- id PK
+-- airline_id FK
+-- departure airport TEXT
+-- landing airport TEXT
+-- departure_datetime TEXT
+-- arrival_datetime TEXT
 
--- airline_concourses:
-    -- id PK
-    -- airline_id FK
-    -- concours
