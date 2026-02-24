a47aaa54fc7248b4b4c681001a80592acd607285
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-02-24 22:43:48 +0200

    Refactor SQL import script to use double quotes for identifiers and improve readability

--- problems/Lecture 3 - Writing/meteorites/import.sql
+++ problems/Lecture 3 - Writing/meteorites/import.sql
@@ -1,34 +1,34 @@
 -- create table "meteorites_temp"
     CREATE TABLE IF NOT EXISTS "meteorites_temp"(
-        name TEXT,
-        id INTEGER,
-        nametype TEXT,
-        class TEXT,
-        mass REAL,
-        discovery TEXT,
-        year INTEGER,
-        lat REAL,
-        long REAL,
+        "name" TEXT,
+        "id" INTEGER,
+        "nametype" TEXT,
+        "class" TEXT,
+        "mass" REAL,
+        "discovery" TEXT,
+        "year" INTEGER,
+        "lat" REAL,
+        "long" REAL,
         PRIMARY KEY ("id")
     );
 
 -- import data from CSV:
--- .import --csv --skip 1 meteorites.csv meteorites_temp
--- SELECT * FROM meteorites_temp; -- data imported from terminal
+-- .import --csv --skip 1 meteorites.csv "meteorites_temp"
+-- SELECT * FROM "meteorites_temp"; -- data imported from terminal
 
 -- update empty to be NULL values
-UPDATE meteorites_temp SET mass = NULL WHERE mass = '';
-UPDATE meteorites_temp SET year = NULL WHERE year ='';
-UPDATE meteorites_temp SET lat = NULL WHERE lat = '';
-UPDATE meteorites_temp SET long = NULL WHERE long = '';
+UPDATE "meteorites_temp" SET "mass" = NULL WHERE "mass" = '';
+UPDATE "meteorites_temp" SET "year" = NULL WHERE "year" ='';
+UPDATE "meteorites_temp" SET "lat" = NULL WHERE "lat" = '';
+UPDATE "meteorites_temp" SET "long" = NULL WHERE "long" = '';
 
 -- ROUND values to TWO digits
-UPDATE meteorites_temp SET mass = ROUND(mass,2)  WHERE mass IS NOT NULL;
-UPDATE meteorites_temp SET lat = ROUND(lat,2)  WHERE lat IS NOT NULL;
-UPDATE meteorites_temp SET long = ROUND(long,2) WHERE long IS NOT NULL;
+UPDATE "meteorites_temp" SET "mass" = ROUND("mass",2)  WHERE "mass" IS NOT NULL;
+UPDATE "meteorites_temp" SET "lat" = ROUND("lat",2)  WHERE "lat" IS NOT NULL;
+UPDATE "meteorites_temp" SET "long" = ROUND("long",2) WHERE "long" IS NOT NULL;
 
 -- delete Relict FROM meteorites_temp Table
-DELETE FROM meteorites_temp WHERE nametype = 'Relict';
+DELETE FROM "meteorites_temp" WHERE "nametype" = 'Relict';
 
 -- Create meteorites table :
 CREATE TABLE IF NOT EXISTS "meteorites" (
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS "meteorites" (
 
 INSERT INTO meteorites ( name , class , mass, discovery,year,lat , long)
 SELECT  name, class, mass, discovery, year, lat, long
-FROM meteorites_temp
+FROM "meteorites_temp"
 ORDER BY year ASC , name ASC;
 
-DROP TABLE meteorites_temp;
+DROP TABLE "meteorites_temp";
