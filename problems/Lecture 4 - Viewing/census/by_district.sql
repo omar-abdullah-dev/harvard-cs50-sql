d5655a299c3cfc82db6f6e76cf0769c493d0a3f7
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-02-28 01:50:13 +0200

    Refactor SQL view for census data by district to improve readability

--- problems/Lecture 4 - Viewing/census/by_district.sql
+++ problems/Lecture 4 - Viewing/census/by_district.sql
@@ -3,6 +3,6 @@ SELECT census.district,SUM(families) AS families ,
        SUM(households) AS households,
        SUM(population) AS population,
        SUM(male) AS male,
-       SUM(female) AS female FROM census GROUP BY district;
-
-
+       SUM(female) AS female
+FROM census
+GROUP BY district;
