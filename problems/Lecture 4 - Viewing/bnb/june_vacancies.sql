f1ec4ee8834f6e2a4bc88d4f3a06eb1cdf30c91c
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-03-01 15:50:08 +0200

    Add SQL views for June vacancies and listings without descriptions

---
+++ problems/Lecture 4 - Viewing/bnb/june_vacancies.sql
@@ -0,0 +1,11 @@
+CREATE VIEW june_vacancies AS
+SELECT listings.id,listings.property_type,listings.host_name, COUNT(*) AS days_vacant
+FROM listings
+         LEFT JOIN availabilities ON listings.id = availabilities.listing_id
+    AND availabilities.available = 1
+    AND date >= '2023-06-01'
+    AND date < '2023-07-01'
+GROUP BY listings.id,
+         listings.property_type,
+         listings.host_name;
+
--- problems/Lecture 4 - Viewing/bnb/no_descriptions.sql
+++ problems/Lecture 4 - Viewing/bnb/no_descriptions.sql
@@ -1,5 +1,3 @@
 CREATE VIEW no_descriptions AS
     SELECT id,property_type,host_name,accommodates,bedrooms
     FROM  listings;
-
-SELECT * FROM no_descriptions;
