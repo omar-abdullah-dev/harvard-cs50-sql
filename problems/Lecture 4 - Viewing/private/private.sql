fab58a558df9ab63bc87a37e2b6e014f0077b617
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-03-01 03:47:23 +0200

    Add cipher table and message view to enhance sentence processing

---
+++ problems/Lecture 4 - Viewing/private/private.sql
@@ -0,0 +1,19 @@
+CREATE TABLE cipher (
+    sentence_id INTEGER,
+    start_char INTEGER ,
+    length INTEGER
+);
+
+INSERT INTO cipher VALUES (14,98,4),
+                          (114,3,5),
+                          (618,72,9),
+                          (630,7,3),
+                          (932,12,5),
+                          (2230,50,7),
+                          (2346,44,10),
+                          (3041,14,5);
+
+CREATE VIEW message AS
+    SELECT substr(sentence,cipher.start_char,cipher.length) AS 'phrase'
+    FROM sentences
+JOIN cipher ON sentences.id= cipher.sentence_id;
